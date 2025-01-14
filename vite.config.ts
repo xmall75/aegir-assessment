71ed34fdc0d20f091781becb92d778044f6e89b3
Revanantyo D <xmall75.id@gmail.com>
2025-01-14 13:53:30 +0700

    feat: add directus package, add proxy to directus' url to avoid CORS, update env example

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()]
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    define: {
+      'process.env': env
+    },
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.DIRECTUS_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    }
+  }
 })
